be0b823700c7ff66648bce4fb66cf37c59ea1815
Lari Rauno <tuutti@iki.fi>
2016-09-04 02:40:50 +0300

    redirect back to tupas initialize page rather than front

--- modules/tupas_registration/src/Controller/RegistrationController.php
+++ modules/tupas_registration/src/Controller/RegistrationController.php
@@ -19,9 +19,9 @@ class RegistrationController extends SessionController {
   public function register() {
     // Make sure user has active TUPAS session.
     if (!$this->sessionManager->getSession($this->currentUser()->id())) {
-      drupal_set_message($this->t('TUPAS session not found.'));
-
-      return $this->redirect('<front>');
+      drupal_set_message($this->t('TUPAS session not found.'), 'error');
+      // Return to tupas initialize page.
+      return $this->redirect('tupas_session.front');
     }
     // Show map account confirmation form if user is already logged in.
     if ($this->currentUser()->isAuthenticated()) {
